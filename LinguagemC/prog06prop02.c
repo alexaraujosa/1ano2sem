50f36a784de74172c13b5f3cc38cc1a615cc0695
alexaraujosa <a104257@alunos.uminho.pt>
2023-03-05 22:59:30 +0000

    feat(c6): adicionados alguns programas propostos do Capítulo 6
    == CAPÍTULO 6
    
    - Três programas propostos do Capítulo 6 adicionados.

---
+++ LinguagemC/prog06prop02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <mem.h>
+
+char * memset(char *[], char ch, int n)
+{
+    int i;
+    for(i=0 ; i<n ; i++)
+    {
+        v[i] = ch;
+    }
+    return v;
+}
+
+int main()
+{
+    return 0;
+}
